6ec16312226563c12eec661a095a9dd97d985797
trong123432 <5001104170@student.hcmue.edu.vn>
2025-04-28 13:55:48 +0700

    Update bai5_trang63-64.cpp

--- bai5_trang63-64.cpp
+++ bai5_trang63-64.cpp
@@ -4,65 +4,60 @@ using namespace std;
 
 class Point {
 private:
-    int x;
-    int y;
-    friend class TamGiac;
+    int x, y;
 public:
-    Point () {
+    Point() {
         x = 0;
         y = 0;
-    };
-    Point (int x, int y) {
+    }
+    Point(int x, int y) {
         this->x = x;
         this->y = y;
     }
-
     void print_point() {
-        cout<<"("<<x<<","<<y<<")"<<endl;
+        cout << "(" << x << "," << y << ")";
     }
+    int getX() const { return x; }
+    int getY() const { return y; }
 };
 
 class TamGiac {
 private:
     Point A, B, C;
 public:
-    TamGiac () {};
-    ~TamGiac () {};
-
+    TamGiac() {}
+    ~TamGiac() {}
     void set(int x1, int y1, int x2, int y2, int x3, int y3) {
-        A = Point(x1,y1);
-        B = Point(x2,y2);
-        C = Point(x3,y3);
+        A = Point(x1, y1);
+        B = Point(x2, y2);
+        C = Point(x3, y3);
     }
-    //khoang cach
     float distance(const Point& p1, const Point& p2) const {
-        int dx = p2.x - p1.x;
-        int dy = p2.y - p1.y;
+        int dx = p2.getX() - p1.getX();
+        int dy = p2.getY() - p1.getY();
         return sqrt(dx * dx + dy * dy);
     }
-    //chu vi
     float perimeter() const {
         float AB = distance(A, B);
         float BC = distance(B, C);
         float CA = distance(C, A);
-        float sum = AB + BC + CA;
-        return sum;
+        return AB + BC + CA;
     }
-    //dien tich
     float area_calculation() const {
-        float area = 0.5 * abs(A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y));
+        float area = 0.5 * abs(A.getX() * (B.getY() - C.getY()) + 
+                              B.getX() * (C.getY() - A.getY()) + 
+                              C.getX() * (A.getY() - B.getY()));
         return area;
     }
-
     void print() {
         cout << "Tam giac co 3 dinh:" << endl;
         cout << "A"; A.print_point(); cout << endl;
         cout << "B"; B.print_point(); cout << endl;
         cout << "C"; C.print_point(); cout << endl;
-        cout<<"Chu vi tam giac: "<<perimeter()<<endl;
-        cout<<"Dien tich tam giac: "<<area_calculation()<<endl;
+        cout << "Chu vi tam giac: " << perimeter() << endl;
+        cout << "Dien tich tam giac: " << area_calculation() << endl;
         int type = classify();
-        cout<<"thuoc tam giac: ";
+        cout << "thuoc tam giac: ";
         switch(type) {
             case 0: cout << "Tam giac thuong"; break;
             case 1: cout << "Tam giac vuong"; break;
@@ -70,33 +65,28 @@ public:
             case 3: cout << "Tam giac vuong can"; break;
             case 4: cout << "Tam giac deu"; break;
         }
+        cout << endl;
     }
-
     int classify() {
         float AB = distance(A, B);
         float BC = distance(B, C);
         float CA = distance(C, A);
-        //tam giac deu
         if (fabs(AB - BC) < 1e-6 && fabs(BC - CA) < 1e-6) {
             return 4;
         }
-        // Tam giac vuong can
-        else if ((fabs(AB*AB + BC*BC - CA*CA) < 1e-6 && AB == BC) ||
-              (fabs(BC*BC + CA*CA - AB*AB) < 1e-6 && BC == CA) ||
-              (fabs(CA*CA + AB*AB - BC*BC) < 1e-6 && AB == CA)) {
+        else if ((fabs(AB*AB + BC*BC - CA*CA) < 1e-6 && fabs(AB - BC) < 1e-6) ||
+                 (fabs(BC*BC + CA*CA - AB*AB) < 1e-6 && fabs(BC - CA) < 1e-6) ||
+                 (fabs(CA*CA + AB*AB - BC*BC) < 1e-6 && fabs(CA - AB) < 1e-6)) {
             return 3;
         }
-        // Tam giac vuong
         else if (fabs(AB*AB + BC*BC - CA*CA) < 1e-6 ||
-            fabs(BC*BC + CA*CA - AB*AB) < 1e-6 ||
-            fabs(CA*CA + AB*AB - BC*BC) < 1e-6) {
+                 (fabs(BC*BC + CA*CA - AB*AB) < 1e-6) ||
+                 (fabs(CA*CA + AB*AB - BC*BC) < 1e-6)) {
             return 1;
         }
-        // Tam giac cân
         else if (fabs(AB - BC) < 1e-6 || fabs(BC - CA) < 1e-6 || fabs(CA - AB) < 1e-6) {
             return 2;
         }
-        //tam giac thuong
         else {
             return 0;
         }
@@ -105,7 +95,7 @@ public:
 
 int main() {
     TamGiac tam_giac1;
-    tam_giac1.set(0, 0, 2, 0, 1,1.73205);
+    tam_giac1.set(0, 0, 2, 0, 1, 1);
     tam_giac1.print();
     return 0;
 }
